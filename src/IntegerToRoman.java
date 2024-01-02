d2166249efce33b52f64e7170879d1b026382f80
Meet <katrodiya.m@northeastern.edu>
2024-01-01 23:58:43 -0500

    Added solution for Integer to Roman

---
+++ src/IntegerToRoman.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class IntegerToRoman {
+  public String intToRoman(int num) {
+    Map<Integer, String> numMap = new HashMap<>();
+    numMap.put(1, "I");
+    numMap.put(5, "V");
+    numMap.put(10, "X");
+    numMap.put(50, "L");
+    numMap.put(100, "C");
+    numMap.put(500, "D");
+    numMap.put(1000, "M");
+    numMap.put(4, "IV");
+    numMap.put(9, "IX");
+    numMap.put(40, "XL");
+    numMap.put(90, "XC");
+    numMap.put(400, "CD");
+    numMap.put(900, "CM");
+
+    int[] divisors = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    int temp = num;
+    StringBuilder result = new StringBuilder();
+    int i = 0;
+    while (temp != 0) {
+      while (i < divisors.length && (temp / divisors[i] == 0)) {
+        i++;
+      }
+      result.append(numMap.get(divisors[i]));
+      temp %= divisors[i];
+    }
+    return result.toString();
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +5,7 @@ public class Main {
         int[][] input2d = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         char[] charInput = new char[]{'c','f','j'};
         String strInput = "";
+        int intInput = 58;
 //        ListNode node4 = new ListNode(4);
 //        ListNode node2 = new ListNode(2);
 //        ListNode node1 = new ListNode(1);
@@ -14,6 +14,6 @@ public class Main {
 //        node2.next = node1;
 //        node1.next = node3;
         new RotateImage().rotate(input2d);
-        System.out.println("answer: " + Arrays.deepToString(input2d));
+        System.out.println("answer: " + new IntegerToRoman().intToRoman(intInput));
     }
 }
